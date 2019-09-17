96963d1348d2e129dff7280bdf2da5a37313723b
Your Name <jsg2145@cumc.columbia.edu>
2019-09-17 10:39:47 -0400

    add another dataset

--- 20190917.R
+++ 20190917.R
@@ -1,3 +1,7 @@
 library(tidyverse)
 litters_data = read_csv(file = "./data_import_examples/data_import_examples/FAS_litters.csv")
 litters_data
+litters_data = janitor::clean_names(litters_data)
+litters_data
+fas_pups = read_csv(file = "./data_import_examples/data_import_examples/FAS_pups.csv")
+fas_pups
