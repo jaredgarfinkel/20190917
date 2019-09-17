573daecec49302ddf8365437ef5f126b62a36202
Your Name <jsg2145@cumc.columbia.edu>
2019-09-17 11:01:48 -0400

    add in an excel and a sas file

--- 20190917.R
+++ 20190917.R
@@ -5,3 +5,10 @@ litters_data = janitor::clean_names(litters_data)
 litters_data
 fas_pups = read_csv(file = "./data_import_examples/data_import_examples/FAS_pups.csv")
 fas_pups
+fas_pups = janitor::clean_names(fas_pups)
+view(fas_pups)
+library(readxl)
+mlb11_data = read_excel(path = "./data_import_examples/data_import_examples/mlb11.xlsx")
+view(mlb11_data)
+pulse_data = haven::read_sas("./data_import_examples/data_import_examples/public_pulse_data.sas7bdat")
+view(pulse_data)
